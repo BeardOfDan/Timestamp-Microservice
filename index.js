e04fe6f49542c7a27528ee4d60694a11ff75387e
Daniel Roberts <BeardOfDan@gmail.com>
2018-03-27 16:06:21 -0700

    (refactor) Change time to date
    Technically these are dates, not times, so this is more accurate

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ app.get('/', (req, res, next) => {
   res.status(200).sendFile(__dirname + 'public/index.html');
 });
 
-app.get('/:time', (req, res, next) => {
-  const time = req.params.time;
+app.get('/:date', (req, res, next) => {
+  const date = req.params.date;
   const result = { 'unix': null, 'natural': null };
 
-  const unix = moment.unix(time);
-  const natural = moment(time, ['MMMM DD, YYYY', 'MMMM D, YYYY']);
+  const unix = moment.unix(date);
+  const natural = moment(date, 'MMMM D, YYYY');
 
   if (unix.isValid()) {
     result.unix = unix.unix();
-    result.natural = unix.utc().format('MMMM D, YYYY').toString();
+    result.natural = unix.format('MMMM D, YYYY').toString();
   } else if (natural.isValid()) {
     result.unix = natural.unix();
     result.natural = natural.format('MMMM D, YYYY').toString();
