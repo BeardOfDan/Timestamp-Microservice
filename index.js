be40172bd3df1d5ec0720ff99b6ec1e669f431b6
Daniel Roberts <BeardOfDan@gmail.com>
2018-03-27 15:19:38 -0700

    (feat) Add moment js and get route handler for /:time

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
+const moment = require('moment')
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,6 +10,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res, next) => {
   res.status(200).sendFile(__dirname + 'public/index.html');
+});
+
+app.get('/:time', (req, res, next) => {
+  const time = req.params.time;
+  const result = { 'unix': null, 'natural': null };
+  if (moment(time).isValid() || (false)) {
+    result.unix = '';
+    result.natural = '';
+  }
+  res.send(result);
 });
 
 app.listen(PORT, () => {
